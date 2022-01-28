33b553941b1a2e236de3318ea1cef42c17caf359
noah elmont <2047541@talnet.nl>
2022-01-28 10:41:50 +0100

    eindopdracht

--- Eindopdracht/bitcoinData.php
+++ Eindopdracht/bitcoinData.php
@@ -18,7 +18,7 @@
 /* 
  * Opdracht 2: Zet hier de $buttons array neer 
  */
-
+$buttons = array(400, 500, 750, 1000, 1500);
 
 /******************************************* OPDRACHT 2 doe je hierboven *******************************************/
 /*******************************************************************************************************************/
@@ -41,8 +41,7 @@ function calculateBitcoinAmount()
     }
 
     //SCHRIJF JE CODE.
-
-
+    return $euro/getBitcoinPrice();
 
 }
 /******************************************* OPDRACHT 3 doe je hierboven *******************************************/
@@ -79,6 +78,7 @@ if (isset($_GET['euro'])) {
      * Schrijf hieronder de code om de gekochte bitcoin aan de array $transactions toe te voegen.
      * Je voegt een bitcoinprijs toe, de prijs in euro's en het aantal gekochte bitcoins.
      */
+    array_push($transaction, [getBitcoinPrice(), $euro, calculateBitcoinAmount()]);
 }
 
 
